bd975e8430650e9304e9c38b72ed7b8b8eae7940
Iraponan Marinho <iraponan.aires@gmail.com>
2024-01-20 14:12:13 -0300

    Alguns exemplos de formatação do TextField.

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,33 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-mail',
+              hintText: 'exemplo@exemplo.com',
+              //border: OutlineInputBorder(),
+              //errorText: 'Campo Obirgatório!',
+              //prefixText: 'R\$ ',
+              //suffixText: ' cm',
+              /*labelStyle: TextStyle(
+                fontSize: 20,
+              ),*/
+            ),
+            //obscureText: true,
+            //obscuringCharacter: 'x',
+            keyboardType: TextInputType.emailAddress,
+            /*style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.purple
+            ),*/
+          ),
+        ),
+      ),
     );
   }
 }
